d3a3761c4f29caec07c146dc6ab01c5189bc5ab9
Sami <hellosami@protonmail.com>
2023-09-20 13:53:48 +0200

    Add enough code to fix first failing test

--- src/main/java/org/example/MorseInterpreter.java
+++ src/main/java/org/example/MorseInterpreter.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class MorseInterpreter {
     public String interpret(String s) {
+        if(s.equals("*-")) {
+            return "A";
+        }
         return "test";
     }
 }
