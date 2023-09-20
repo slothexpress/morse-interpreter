015cd9b304b372f318291c2318e285963f8243c5
Sami <hellosami@protonmail.com>
2023-09-20 15:04:17 +0200

    Add scanner to main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String result = "";
+        MorseInterpreter morse = new MorseInterpreter();
+
+        System.out.println("SELECT YOUR CHOICE OF INTERPETATION. 1 = TO MORSE CODE, 2 = FROM MORSE CODE");
+        Scanner scan = new Scanner(System.in);
+        int option = scan.nextInt();
+        scan.nextLine(); // Consume the newline character and clear scanner input
+
+        System.out.println("ENTER SEQUENCE TO TRANSLATE: ");
+        String input = scan.nextLine();
+
+        switch (option) {
+            case 1: result = morse.interpretToMorseCode(input);
+                break;
+            case 2: result = morse.interpretFromMorseCode(input);
+                break;
+            default:
+                System.out.println("INVALID CHOICE");
+                break;
+        }
+
+        System.out.println(result);
     }
 }
