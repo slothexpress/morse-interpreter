df284692e82604a8b529a687ee2dabc20f92ea80
Sami <hellosami@protonmail.com>
2023-10-06 14:53:14 +0200

    Add service class

---
+++ src/main/java/org/example/MorseService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MorseService {
+
+    @Autowired
+    public MorseService() {
+    }
+}
